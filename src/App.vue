0d4f0d806bb127a796a9a95c1b722a51bfd384d1
Ismail Baisa <maingmeong.gmail.com>
2021-04-15 13:42:53 +0700

    added likes

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,91 @@
 <template>
-  <div id="app">
+<div id="app">
     <detail v-if="modal">
-      <button @click="closeModal">Close</button>
-      <h3>{{ detail.title }}</h3>
-      <img lazy-load :src="detail.image" />
-      <h4>SHOW TIME</h4>
-      <h4>{{ detail.showTime }}</h4>
-      <p>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
-        maiores, aliquam rerum omnis placeat fugiat? Commodi vitae corrupti,
-        eius, nisi consequuntur tempora nobis saepe quos quae totam officia
-        animi vero.
-      </p>
+        <button @click="closeModal">Close</button>
+        <h3>{{ detail.title }}</h3>
+        <img lazy-load :src="detail.image" />
+        <h4>SHOW TIME</h4>
+        <h4>{{ detail.showTime }}</h4>
+        <p>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Minima
+            maiores, aliquam rerum omnis placeat fugiat? Commodi vitae corrupti,
+            eius, nisi consequuntur tempora nobis saepe quos quae totam officia
+            animi vero.
+        </p>
+      <span class="likes flex flex-ai-c flex-jc-e">
+            <span>{{ detail.like}}</span><span class="material-icons love">
+                favorite
+            </span>
+        </span>
     </detail>
     <div class="movies flex flex-jc-c flex-ai-c flex-fd-c" :class="turn">
-      <div>
-        <input type="text" placeholder="Search here..." v-model="search" />
-        <input type="date" v-model="date" />
-      </div>
-      <div class="movies__list flex flex-jc-c flex-ai-c">
-        <div
-          v-for="movie in movies"
-          :key="movie.id"
-          @click="getDetail(movie)"
-          class="flex flex-jc-sb flex-ai-c flex-fd-c"
-        >
-          <h3>{{ movie.title }}</h3>
-          <img lazy-load :src="movie.image" />
-          <h4>SHOW TIME</h4>
-          <h4>{{ movie.showTime }}</h4>
+        <div>
+            <input type="text" placeholder="Search here..." v-model="search" />
+            <input type="date" v-model="date" />
         </div>
-      </div>
+        <div class="movies__list flex flex-jc-c flex-ai-c">
+            <div v-for="movie in movies" :key="movie.id" @click="getDetail(movie)" class="flex flex-jc-sb flex-ai-c flex-fd-c">
+                <h3>{{ movie.title }}</h3>
+                <img lazy-load :src="movie.image" />
+                <h4>SHOW TIME</h4>
+                <h4>{{ movie.showTime }}</h4>
+                <span class="likes flex flex-ai-c flex-jc-e">
+                    <span>{{ movie.like}}</span><span class="material-icons love">
+                        favorite
+                    </span>
+                </span>
+            </div>
+        </div>
     </div>
-  </div>
+</div>
 </template>
 
 <script>
-import { mapGetters } from "vuex";
+import {
+    mapGetters
+} from "vuex";
 import Detail from "./components/Detail.vue";
 
 export default {
-  components: {
-    Detail,
-  },
-  data() {
-    return {
-      search: "",
-      modal: false,
-      detail: "",
-      date: "",
-      turn: "",
-    };
-  },
-  created() {
-    this.$store.dispatch("loadMovies");
-  },
-  methods: {
-    getDetail(movie) {
-      this.detail = movie;
-      this.modal = true;
-      this.turn = "turn";
+    components: {
+        Detail,
     },
-    closeModal() {
-      this.modal = false;
-      this.turn = "";
+    data() {
+        return {
+            search: "",
+            modal: false,
+            detail: "",
+            date: "",
+            turn: "",
+        };
     },
-  },
-  computed: {
-    ...mapGetters({
-      getMovies: "getMovies",
-    }),
-    movies: function() {
-      return this.getMovies.filter((movie) => {
-        if (this.search) {
-          return movie.title.match(this.search);
-        } else {
-          return movie.showTime.match(this.date);
-        }
-      });
+    created() {
+        this.$store.dispatch("loadMovies");
     },
-  },
+    methods: {
+        getDetail(movie) {
+            this.detail = movie;
+            this.modal = true;
+            this.turn = "turn";
+        },
+        closeModal() {
+            this.modal = false;
+            this.turn = "";
+        },
+    },
+    computed: {
+        ...mapGetters({
+            getMovies: "getMovies",
+        }),
+        movies: function () {
+            return this.getMovies.filter((movie) => {
+                if (this.search) {
+                    return movie.title.match(this.search);
+                } else {
+                    return movie.showTime.match(this.date);
+                }
+            });
+        },
+    },
 };
 </script>
